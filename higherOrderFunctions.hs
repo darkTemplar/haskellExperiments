cf1f95ad7e6341f420ffca6040ba5a9ce338ed4f
Abhas <abhasforyou@gmail.com>
2014-11-30 22:40:53 -0800

    Re-implementing some higher order functions for assignments

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -1,68 +1,35 @@
 import Data.Char
 
--- demo of filter (sum of even numbers using filter)
-sumEven :: [Int] -> Int
-sumEven xs = sum $ filter even xs
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = not . any (not . p)
 
--- define filter recursively
-myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter f [] = []
-myFilter f (x:xs)
-	| f x = x : myFilter f xs
-	| otherwise = myFilter f xs 
+any' :: (a -> Bool) -> [a] -> Bool
+any' p xs = not (all (\ x -> not (p x)) xs)
 
--- demo of Map (convert each char to uppercase using a lambda function
-myUpper :: [Char] -> [Char]
-myUpper xs = map (\x -> toUpper x) xs
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p (x:xs)
+	| p x = x : takeWhile' p xs
+	| otherwise = []
 
--- define map recursively
-myMap :: (a -> b) -> [a] -> [b]
-myMap f [] = []
-myMap f (x:xs) = f x : myMap f xs
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' _ [] = []
+dropWhile' p (x:xs)
+	| p x = dropWhile' p xs
+	| otherwise = x:xs
 
---exercises for Chapter 7 (Programming in Haskell)
--- 7.2 define functions all, any, takeWhile and dropWhile
--- i) all
-myAll :: (a -> Bool) -> [a] -> Bool
-myAll f xs = take 1 ([False | x <- xs, not $ f x]) == [False]
+-- foldl implementation of map
+mapl :: (a->b) -> [a] -> [b]
+mapl f xs = foldl (\ acc x -> acc ++ [f x]) [] xs
 
--- ii) any
-myAny :: (a -> Bool) -> [a] -> Bool
-myAny f xs = take 1 ([True | x <- xs, f x]) == [True]
+-- foldl implementation of filter
+filterl :: (a -> Bool) -> [a] -> [a]
+filterl p xs = foldl (\ acc x -> if p x then acc ++ [x] else acc) [] xs
 
--- iii) takeWhile
-myTakeWhile :: (a -> Bool) -> [a] -> [a]
-myTakeWhile f [] = []
-myTakeWhile f (x:xs)
-	| f x = x : myTakeWhile f xs
-	| otherwise = myTakeWhile f []
-
--- iv) dropWhile
-myDropWhile :: (a -> Bool) -> [a] -> [a]
-myDropWhile f (x:xs)
-	| f x = myDropWhile f xs
-	| otherwise = (x:xs)
-
--- implementing sum using foldr
-mySum :: (Num a) => [a] -> a
-mySum xs = foldr (+) 0 xs
-
--- reverse using foldr
-myReverse :: [a] -> [a]
-myReverse xs = foldr (\x acc -> acc ++ [x]) [] xs
-
--- filter using foldr
-myFilter1 :: (a -> Bool) -> [a] -> [a]
-myFilter1 p xs = foldr (\x acc -> if p x then x:acc else acc) [] xs
-
--- 7.4. convert decimal number to Int => [2,3,4,5] -> 2345
-dec2int :: [Int] => Int
-dec2int xs = foldl (\acc x -> 10*acc + x) 0 xs
+dec2int :: [Int] -> Int
+dec2int xs = foldl (\ acc x -> 10*acc + x) 0 xs
 
 
+compose :: [a -> a] -> (a -> a)
+compose = foldr (.) id 
 
-
-
-
-
-
+sumsqreven = compose [sum, map(^2), filter even] 
