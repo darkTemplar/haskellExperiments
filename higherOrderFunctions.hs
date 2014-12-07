6b959d0f04b8f6c0ca927d72b01a2ca79182753b
Abhas <abhasforyou@gmail.com>
2014-12-07 00:31:46 -0800

    Adding final solutions to assignment 6

--- higherOrderFunctions.hs
+++ higherOrderFunctions.hs
@@ -29,7 +29,33 @@ dec2int :: [Int] -> Int
 dec2int xs = foldl (\ acc x -> 10*acc + x) 0 xs
 
 
-compose :: [a -> a] -> (a -> a)
-compose = foldr (.) id 
+--compose :: [a -> a] -> (a -> a)
+--compose = foldr (.) id 
 
-sumsqreven = compose [sum, map(^2), filter even] 
+--sumsqreven = compose [sum, map(^2), filter even] 
+
+unfold :: (b -> Bool) -> (b -> a) -> (b -> b) -> b -> [a]
+unfold p h t x
+	| p x = []
+	| otherwise = h x : unfold p h t (t x) 
+
+-- function to convert Integer to list of Bits (LSB is at head)
+type Bit = Int
+bin2int :: Int -> [Bit]
+bin2int = unfold (== 0) (`mod` 2) (`div` 2)
+
+-- convert list of bits into lists of at most 8 bits
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+chop8' :: [Bit] -> [[Bit]]
+chop8' = unfold null (take 8) (drop 8)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold null (f . head) tail
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' f = unfold (const False) id f
+
+
